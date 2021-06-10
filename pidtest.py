71a70ca730c0cdb416f71c1669b7d94c65281418
Harsh Mathur <harshmathur.1990@gmail.com>
2021-06-10 13:29:58 +0530

    more

--- pidtest.py
+++ pidtest.py
@@ -1,8 +1,10 @@
 import numpy as np
 
 
-def realistic_pid_controller_simulator_for_autoguider(value1, value2, target1, target2, a1, a2, b1, b2, kp, ki, kd, noise=0, drift=1):
+def realistic_pid_controller_simulator_for_autoguider(value1, value2, target1, target2, a1, a2, b1, b2, kp, ki, kd, seeing_arcsec=0, drift_arcsec=1):
 
+    noise = seeing_arcsec * 223 / 5.5
+    drift = drift_arcsec * 223 / (5.5 * 60)
     errorvoltage1 = 0
     errorvoltage2 = 0
     integral_error1 = 0
@@ -15,7 +17,7 @@ def realistic_pid_controller_simulator_for_autoguider(value1, value2, target1, t
     corr_list1, corr_list2 = list(), list()
     motor_count1, motor_count2 = list(), list()
 
-    for i in range(3600 * 4):
+    for i in range(15 * 60):
         errorvoltage1 = target1 - value1
         errorvoltage2 = target2 - value2
         integral_error1 += errorvoltage1
@@ -26,8 +28,8 @@ def realistic_pid_controller_simulator_for_autoguider(value1, value2, target1, t
         correction_voltage2 = kp * errorvoltage2 + ki * integral_error2 + kd * error_derivative2
         mc1 = correction_voltage1 * a1 + correction_voltage2 * a2
         mc2 = correction_voltage1 * b1 + correction_voltage2 * b2
-        value1 += correction_voltage1 + drift * 4 + np.random.randint(-np.abs(noise * value1), np.abs(noise * value1))
-        value2 += correction_voltage2 + drift * 4 + np.random.randint(-np.abs(noise * value2), np.abs(noise * value2))
+        value1 += correction_voltage1 + drift + np.random.randint(-noise, noise)
+        value2 += correction_voltage2 + drift + np.random.randint(-noise, noise)
         res_list1.append(value1)
         res_list2.append(value2)
         corr_list1.append(correction_voltage1)
