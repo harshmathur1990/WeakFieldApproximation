f429e126279cd2bf2fb686693a814e56631dd094
Harsh Mathur <harshmathur.1990@gmail.com>
2021-09-22 21:33:28 +0530

    write bifrost to rh15d

---
+++ generate_bifrost_rh15d_model.py
@@ -0,0 +1,168 @@
+import numpy as np
+import sunpy.io.fits
+from helita.sim import rh15d
+from pathlib import Path
+
+
+def make_atmosphere(
+    foldername,
+    simulation_name,
+    snap,
+    start_x, end_x,
+    start_y, end_y,
+    height_min_in_m,
+    height_max_in_m
+):
+    if isinstance(foldername, str):
+        foldername = Path(foldername)
+
+    temp_file = 'BIFROST_{}_lgtg_{}.fits'.format(
+        simulation_name,
+        snap
+    )
+
+    data, header = sunpy.io.fits.read(
+        foldername / temp_file
+    )[0]
+
+    height, _ = sunpy.io.fits.read(
+        foldername / temp_file
+    )[1]
+
+    height = height * 1e6
+
+    ind = np.where((height >= height_min_in_m) & (height <= height_max_in_m))[0]
+
+    T = np.zeros((1, end_x - start_x, end_y - start_y, ind.size))
+
+    T[0] = np.pow(
+        np.transpose(
+            data[ind, start_x:end_x, start_y:end_y],
+            axes=(1, 2, 0)
+        ),
+        10
+    )
+
+    vz_file = 'BIFROST_{}_uz_{}.fits'.format(
+        simulation_name, snap
+    )
+
+    data, header = sunpy.io.fits.read(
+        foldername / vz_file
+    )[0]
+
+    vz = np.zeros((1, end_x - start_x, end_y - start_y, ind.size))
+
+    vz[0] = np.transpose(
+        data[ind, start_x:end_x, start_y:end_y],
+        axes=(1, 2, 0)
+    )
+
+    z = np.zeros((1, end_x - start_x, end_y - start_y, ind.size))
+
+    z[:, :, :, :] = height[ind]
+
+    nH = np.zeros((1, 6, end_x - start_x, end_y - start_y, ind.size))
+
+    for i in range(1, 7):
+        nhfile = 'BIFROST_{}_lgn{}_{}.fits'.format(
+            simulation_name, i, snap
+        )
+
+        data, header = sunpy.io.fits.read(
+            foldername / nhfile
+        )[0]
+
+        nH[0, i-1] = np.pow(
+            np.transpose(
+                data[ind, start_x:end_x, start_y:end_y],
+                axes=(1, 2, 0)
+            ),
+            10
+        )
+
+    nefile = 'BIFROST_{}_lgne_{}.fits'.format(
+        simulation_name, snap
+    )
+
+    data, header = sunpy.io.fits.read(
+            foldername / nefile
+        )[0]
+
+    ne = np.zeros((1, end_x - start_x, end_y - start_y, ind.size))
+
+    ne[0] = np.pow(
+        np.transpose(
+            data[ind, start_x:end_x, start_y:end_y],
+            axes=(1, 2, 0)
+        ),
+        10
+    )
+
+    bxfile = 'BIFROST_{}_bx_{}.fits'.format(
+        simulation_name, snap
+    )
+
+    data, header = sunpy.io.fits.read(
+            foldername / bxfile
+        )[0]
+
+    Bx = np.zeros((1, end_x - start_x, end_y - start_y, ind.size))
+
+    Bx[0] = np.transpose(
+        data[ind, start_x:end_x, start_y:end_y],
+        axes=(1, 2, 0)
+    )
+
+    byfile = 'BIFROST_{}_by_{}.fits'.format(
+        simulation_name, snap
+    )
+
+    data, header = sunpy.io.fits.read(
+            foldername / byfile
+        )[0]
+
+    By = np.zeros((1, end_x - start_x, end_y - start_y, ind.size))
+
+    By[0] = np.transpose(
+        data[ind, start_x:end_x, start_y:end_y],
+        axes=(1, 2, 0)
+    )
+
+    bzfile = 'BIFROST_{}_bz_{}.fits'.format(
+        simulation_name, snap
+    )
+
+    data, header = sunpy.io.fits.read(
+            foldername / bzfile
+        )[0]
+
+    Bz = np.zeros((1, end_x - start_x, end_y - start_y, ind.size))
+
+    Bz[0] = np.transpose(
+        data[ind, start_x:end_x, start_y:end_y],
+        axes=(1, 2, 0)
+    )
+
+    T = T[:, :, :, ::-1]
+    vz = vz[:, :, :, ::-1]
+    z = z[:, :, :, ::-1]
+    nH = nH[:, :, :, :, ::-1]
+    ne = ne[:, :, :, ::-1]
+    Bx = Bx[:, :, :, ::-1]
+    By = By[:, :, :, ::-1]
+    Bz = Bz[:, :, :, ::-1]
+
+    outfile = 'bifrost_{}_{}_{}_{}_{}_{}_{}.nc'.format(
+        simulation_name, start_x, end_x, start_y, end_y, height_min_in_m, height_max_in_m
+    )
+
+    make_xarray_atmos(
+        outfile, T, vz, z,
+        nH=nH, Bz=Bz, By=By,
+        Bx=Bx, ne=ne,
+        desc='Bifrost Simulation (simulation_name, start_x, end_x, start_y, end_y, height_min_in_m, height_max_in_m) - {} - {} - {} - {} - {} - {} - {}'.format(
+            simulation_name, start_x, end_x, start_y, end_y, height_min_in_m, height_max_in_m
+        ),
+        snap=snap
+    )
