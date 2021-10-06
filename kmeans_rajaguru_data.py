2c35a4e385c58b520b608b95ab835d1222771b7b
Harsh Mathur <harsh.mathur@iiap.res.in>
2021-10-06 16:52:23 +0530

    more work

--- kmeans_rajaguru_data.py
+++ kmeans_rajaguru_data.py
@@ -85,7 +85,7 @@ def do_work(num_clusters):
         fout['inertia_'] = model.inertia_
         fout['n_iter_'] = model.n_iter_
 
-        rps = np.zeros((23, 5), dtype=np.float64)
+        rps = np.zeros((num_clusters, 23, 5), dtype=np.float64)
 
         framerows /= weights
         framerows = (framerows * sd) - mn
