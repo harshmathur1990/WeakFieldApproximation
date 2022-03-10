e6dd505a12536340bc48a88bbe1363ff0ec306bc
Harsh Mathur <harsh.mathur@iiap.res.in>
2022-03-10 14:10:49 +0530

    more

--- make_fov_result_plots.py
+++ make_fov_result_plots.py
@@ -16,6 +16,7 @@ wfa_8542_data_file = processed_inputs / 'wfa_8542.nc'
 
 def get_fov_data():
     data = np.zeros((3, 2, 17, 60), dtype=np.float64)
+    mask = np.zeros((3, 2, 17, 60), dtype=np.int64)
 
     fcaha = h5py.File(ca_ha_data_file, 'r')
 
@@ -40,6 +41,10 @@ def get_fov_data():
     data[2][0] = fme['B_abs'][()] * np.cos(fme['inclination_rad'][()])
     data[2][1] = fwfa['blos_gauss'][()]
 
+    mask[0, 0] = mask[0, 1] = data[1, 0] < (data[1, 0].mean() + (data[1, 0].std() * (-1.9)))
+    mask[1, 0] = mask[1, 1] = data[1, 0] < (data[1, 0].mean() + (data[1, 0].std() * (-1.9)))
+    mask[2, 0] = mask[1, 0]
+    mask[2, 1] = mask[0, 0]
     aa, bb, cc, dd = fcaha['wav'][ind_8542_wing], fcaha['wav'][ind_8542_core], fcaha['wav'][ind_6563_wing], fcaha['wav'][ind_6563_core]
 
     fwfa.close()
@@ -48,15 +53,17 @@ def get_fov_data():
 
     fcaha.close()
 
-    return data, aa, bb, cc, dd
+    return data, aa, bb, cc, dd, mask
 
 
 def make_fov_plots():
-    data, wing_ca, core_ca, wing_ha, core_ha = get_fov_data()
+    data, wing_ca, core_ca, wing_ha, core_ha, mask = get_fov_data()
 
     fig, axs = plt.subplots(3, 2, figsize=(7, 3.3))
 
     extent = [0, 22.8, 0, 6.46]
+
+    fontsize = 8
 
     for i in range(3):
         for j in range(2):
@@ -65,10 +72,17 @@ def make_fov_plots():
                     vmin = -600
                     vmax = 600
                 else:
-                    vmin=
-                axs[i][j].imshow(data[i][j], cmap='RdGy', origin='lower', extent=extent)
+                    vmin = -400
+                    vmax = 400
+
+                gh = axs[i][j].imshow(data[i][j], cmap='RdGy', origin='lower', extent=extent, vmin=vmin, vmax=vmax)
+                if j == 0:
+                    im20 = gh
+                else:
+                    im21 = gh
             else:
-                axs[i][j].imshow(data[i][j], cmap='RdGy', origin='lower', extent=extent)
+                axs[i][j].imshow(data[i][j], cmap='gray', origin='lower', extent=extent)
+            axs[i][j].contour(mask[i][j], levels=0, extent=extent, origin='lower', colors='blue')
 
     axs[0][0].text(
         0.01, 0.8,
@@ -76,13 +90,15 @@ def make_fov_plots():
             np.round(wing_ca - 8542.09), 2
         ),
         transform=axs[0][0].transAxes,
-        color='black'
+        color='black',
+        fontsize=fontsize
     )
     axs[0][1].text(
         0.01, 0.8,
         r'(b) Ca II 8542 $\mathrm{{\AA}}$ core',
         transform=axs[0][1].transAxes,
-        color='white'
+        color='white',
+        fontsize=fontsize
     )
     axs[1][0].text(
         0.01, 0.8,
@@ -90,44 +106,65 @@ def make_fov_plots():
             np.round(wing_ha - 6562.8), 2
         ),
         transform=axs[1][0].transAxes,
-        color='black'
+        color='black',
+        fontsize=fontsize
     )
     axs[1][1].text(
         0.01, 0.8,
         r'(d) H$\alpha$ core',
         transform=axs[1][1].transAxes,
-        color='white'
+        color='white',
+        fontsize=fontsize
     )
-    im20 = axs[2][0].text(
+    axs[2][0].text(
         0.01, 0.8,
         r'(e) $B_{{\mathrm{LOS}}}$ (Fe I 6569 $\AA$) [G]',
         transform=axs[2][0].transAxes,
-        color='black'
+        color='black',
+        fontsize=fontsize
     )
 
-    im21 = axs[2][1].text(
+    axs[2][1].text(
         0.01, 0.8,
-        r'(e) $B_{{\mathrm{LOS}}}$ (Ca II 8542 $\AA$) [G]',
+        r'(f) $B_{{\mathrm{LOS}}}$ (Ca II 8542 $\AA$) [G]',
         transform=axs[2][1].transAxes,
-        color='black'
+        color='black',
+        fontsize=fontsize
     )
 
     cbaxes = inset_axes(
         axs[2][0],
-        width="50%",
+        width="30%",
         height="8%",
         loc=1,
-        borderpad=1
+        borderpad=0.8
     )
     cbar = fig.colorbar(
-        im,
+        im20,
         cax=cbaxes,
-        ticks=[-300, 0, 300],
+        ticks=[-600, 0, 600],
         orientation='horizontal'
     )
 
     # cbar.ax.xaxis.set_ticks_position('top')
-    cbar.ax.tick_params(labelsize=8, colors='white')
+    cbar.ax.tick_params(labelsize=8, colors='black')
+
+    cbaxes = inset_axes(
+        axs[2][1],
+        width="30%",
+        height="8%",
+        loc=1,
+        borderpad=0.8
+    )
+    cbar = fig.colorbar(
+        im21,
+        cax=cbaxes,
+        ticks=[-400, 0, 400],
+        orientation='horizontal'
+    )
+
+    # cbar.ax.xaxis.set_ticks_position('top')
+    cbar.ax.tick_params(labelsize=fontsize, colors='black')
 
     axs[0][0].yaxis.set_minor_locator(MultipleLocator(1))
     axs[0][1].yaxis.set_minor_locator(MultipleLocator(1))
@@ -176,7 +213,10 @@ def make_fov_plots():
     axs[2][1].set_xlabel('x [arcsec]')
 
     plt.subplots_adjust(left=0.05, bottom=0.13, right=1, top=1, hspace=0.0, wspace=0.0)
-    plt.show()
+    # plt.show()
+
+    write_path = Path('/home/harsh/Spinor Paper/')
+    fig.savefig(write_path / 'FOV.pdf', format='pdf', dpi=300)
 
 
 if __name__ == '__main__':
